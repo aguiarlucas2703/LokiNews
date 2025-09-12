136cc5d9c9445894c6e3210d97157423f1d3b6db
hilquias <hilquiassantana0@gmail.com>
2025-09-11 19:55:03 -0500

    update: atualizei as seções de rodapé e de destaques

--- home.js
+++ home.js
@@ -1,1 +1,15 @@
-alert("páia")
+
+const backToTopButton = document.getElementById("back-to-top-btn");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
